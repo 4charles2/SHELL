8eb2960da709c7b534a77b5eac1359f351d95610
4charles2 <ctognol@laposte.net>
2016-05-31 18:36:45 +0200

    exercice script shell revoie communication entre shell et demon"

--- mes_script/git.bash
+++ mes_script/git.bash
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-$reponse=
+func mygit
+{
+	$reponse=
 
-echo "Voici la liste des fichiers qui va être push : "
-git status
+	echo "Voici la liste des fichiers qui va être push : "
+	git status
 
-while true ; do
-	echo "Confirmer l'envoie : "
-	read reponse
-	echo $echo
-	if [ $reponse = "q" ] ; then
-		exit
-	elif [ $reponse = "y" ] ; then
-
-		echo -n "Entrer le message de commit : "
+	while true ; do
+		echo "Confirmer l'envoie : "
 		read reponse
-		git add -A ; git commit -m "$reponse" ; git push origin master
-	break;
-	else
-		echo "Vous avez entrer un mauvais choix : "
-	fi
-done
+		echo $echo
+		if [ $reponse = "q" ] ; then
+			exit
+		elif [ $reponse = "y" ] ; then
 
-echo "Bonne journée ! "
+			echo -n "Entrer le message de commit : "
+			read reponse
+			git add -A ; git commit -m "$reponse" ; git push origin master
+		break;
+		else
+			echo "Vous avez entrer un mauvais choix : "
+		fi
+	done
+
+	echo "Bonne journée ! "
+	return 0
+}
